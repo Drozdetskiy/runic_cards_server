26977d7e9e16ecc914fd54711ff9238affe3296b
Drozdetskiy <m.drozdetskiy@gmail.com>
2019-05-20 11:32:23 +0300

    Problems with web socket on deployment.

--- runic_game.py
+++ runic_game.py
@@ -10,9 +10,9 @@ import socketio
 
 sio = socketio.AsyncServer()
 
-app = web.Application()
+factory = web.Application()
 
-sio.attach(app)
+sio.attach(factory)
 
 clients = []
 
@@ -39,6 +39,7 @@ class ServerSettings:
     PORT = 8081
 
     PLAYER_HASH = 8
+    PLAYER_HASH_LEN = 11
     STATUS = {
         'in_search': 0,
         'found': 1,
@@ -64,17 +65,34 @@ class WebGame(Game):
             hash_player_1: 1,
             hash_player_2: 2
         }
+        self.rooms = {
+            hash_player_1: None,
+            hash_player_2: None
+        }
         self.created_at = time.time()
+        self.game_history = {}
         super().__init__()
 
-    @property
-    def json_repr(self):
+    def next_turn(self, card_index, i, j, get_player=0):
+        player_number = 1 if self.turn % 2 else 2
+        if self.players[get_player] == player_number:
+            super().next_turn(card_index, i, j, player_number)
+        return True
+
+    def get_rooms(self):
+        return list(self.rooms.values())
+
+    def _do_json(self):
         _res = super().json_repr
         _res["card_queue_1"] = list(range(len(_res["player_1_hand"])))
         _res["card_queue_2"] = list(range(len(_res["player_2_hand"])))
         _res["name_player_1"] = self.name_player_1
         _res["name_player_2"] = self.name_player_2
         return json.dumps(_res)
+
+    @property
+    def json_repr(self):
+        return self.game_history.setdefault(self.turn, self._do_json())
 
 
 class UrlGenerator:
@@ -88,7 +106,6 @@ class UrlGenerator:
         self.hash_player_2 = hash_player_2
 
     def add_game_to_queue(self):
-        print(WebGame("sdf", "sadf").json_repr)
         GameQueue.add(
             self.hash_url,
             WebGame(
@@ -108,7 +125,6 @@ class UrlGenerator:
 
 
 async def add_name(sid, message):
-    print(GameQueue.game_queue)
     client_info = json.loads(message)
     game = GameQueue.game_queue[client_info['hash_url']]
     setattr(
@@ -116,9 +132,6 @@ async def add_name(sid, message):
         f'name_player_{game.players[client_info["hash_player"]]}',
         client_info['name']
     )
-    print(game.players)
-    print(game.name_player_1)
-    print(game.name_player_2)
     await sio.emit(
         'message',
         json.dumps({"status": ServerSettings.STATUS['start_game']}),
@@ -187,8 +200,16 @@ def on_connect(sid, environ):
 
 @sio.on('message', namespace='/game')
 async def game_message(sid, message):
-    hash_url = message[:-11:]
+    hash_url = message[:-ServerSettings.PLAYER_HASH_LEN:]
+    player_hash = message[-ServerSettings.PLAYER_HASH_LEN::]
     try:
+        GameQueue.game_queue[hash_url].rooms[player_hash] = sid
+        await sio.emit(
+            'player_number',
+            GameQueue.game_queue[hash_url].players[player_hash],
+            room=sid,
+            namespace='/game'
+        )
         await sio.emit(
             'message',
             GameQueue.game_queue[hash_url].json_repr,
@@ -204,6 +225,32 @@ async def game_message(sid, message):
         )
 
 
+@sio.on('turn', namespace='/game')
+async def game_message(sid, message):
+    player_turn = json.loads(message)
+    hash_url = player_turn['hash_url'][:-ServerSettings.PLAYER_HASH_LEN:]
+    confirmed_turn = GameQueue.game_queue[hash_url].next_turn(
+        player_turn['card_index'],
+        player_turn['i'],
+        player_turn['j'],
+        player_turn['hash_url'][-ServerSettings.PLAYER_HASH_LEN::]
+    )
+    if confirmed_turn:
+        room1, room2 = GameQueue.game_queue[hash_url].get_rooms()
+        await sio.emit(
+            'message',
+            GameQueue.game_queue[hash_url].json_repr,
+            room=room1,
+            namespace='/game'
+        )
+        await sio.emit(
+            'message',
+            GameQueue.game_queue[hash_url].json_repr,
+            room=room2,
+            namespace='/game'
+        )
+
+
 @sio.on('disconnect', namespace='/game')
 def on_disconnect(sid):
     print('disconnect', sid)
@@ -212,4 +259,4 @@ def on_disconnect(sid):
 # app.add_routes([web.get('/host', print_message)])
 # app.add_routes([web.get('/game', game_message)])
 
-web.run_app(app, host=ServerSettings.HOST, port=ServerSettings.PORT)
+web.run_app(factory, host=ServerSettings.HOST, port=ServerSettings.PORT)
